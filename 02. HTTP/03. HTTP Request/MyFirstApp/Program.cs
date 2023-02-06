e17c212eeb09a9d2c392f5edc05933195ef0ea68
concesar12 <cesar-guerrero@javeriana.edu.co>
2023-02-06 14:26:46 +1300

    Update Program.cs
    just comments related to the video explanation

--- 02. HTTP/03. HTTP Request/MyFirstApp/Program.cs
+++ 02. HTTP/03. HTTP Request/MyFirstApp/Program.cs
@@ -3,8 +3,8 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
-    string path = context.Request.Path;
-    string method = context.Request.Method;
+    string path = context.Request.Path; //this line will give the request URL that is present in the start line
+    string method = context.Request.Method; // this will give me the methos (post/get)
   
     context.Response.Headers["Content-type"] = "text/html";
     await context.Response.WriteAsync($"<p>{path}</p>");
